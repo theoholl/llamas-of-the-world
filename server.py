8110202dfac66883853e541088857154dcb83b08
Theo Hollweg <thollweg@mailbox.org>
2024-01-07 12:32:12 +0100

    Simplify and extract validation methods

--- server.py
+++ server.py
@@ -1,15 +1,34 @@
 from flask import Flask, request, redirect, render_template
 import sqlite3
+from validation import CreditCardValidator as validator
 
 app = Flask(__name__)
 
 # User details
-@app.route("/")
+
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    f = open('user.html')
-    content = f.read()
-    f.close()
-    return content
+    if request.method == 'GET':
+        return render_template('user.html')
+    else:
+        name = request.form["name"]
+        credit_card_number = request.form["credit_card_number"]
+        cvv = request.form["cvv"]
+
+        try:
+            if (validator.validate_card_number(credit_card_number) and validator.validate_cardholder_name(name) and validator.validate_cvv(cvv)):
+                connection = sqlite3.connect('database.db')
+                cur = connection.cursor()
+                cur.execute("INSERT INTO users (name, credit_card_number, cvv) VALUES (?, ?, ?)",
+                            (name, credit_card_number, cvv))
+                connection.commit()
+                connection.close()
+        except Exception as ex:
+            return f"<pre>{ex}</pre>"
+
+        return redirect("/lotwie")
+
 
 @app.route("/lotwie", methods=['GET'])
 def lotw():
@@ -17,70 +36,16 @@ def lotw():
     has_llamas = None
 
     if country:
-        connection = sqlite3.connect('database.db')
-        cur = connection.cursor()
-        result = cur.execute(f"SELECT * FROM countries WHERE country_name = '{country}'").fetchall()
-        has_llamas =  bool(result[0][2])
-        
-    # Return template
-    return render_template('./lotw.html', has_llamas=has_llamas, country=country)
-
-@app.route("/credit-card", methods=['POST'])
-def save_user():
-    name = request.form["name"]
-    credit_card_number = request.form["credit_card_number"]
-    cvv = request.form["cvv"]
-
-    try:
-        if (validate_credit_card(credit_card_number) and validate_user_name(name) and validate_cvv(cvv)):
+        try:
             connection = sqlite3.connect('database.db')
             cur = connection.cursor()
-            cur.execute("INSERT INTO users (name, credit_card_number, cvv) VALUES (?, ?, ?)", (name, credit_card_number, cvv))
-            connection.commit()
-            connection.close()
-    except Exception as ex:
-        return f"<p>{ex}</p>"
-    
-    return redirect("/lotwie")
 
+            # Vulnerable to SQL injections
+            result = cur.execute(
+                f"SELECT * FROM countries WHERE country_name = '{country}'").fetchall()
+            has_llamas = bool(result[0][2])
+        except Exception as ex:
+            return f"<pre>{ex}</pre>"
 
-def validate_credit_card(card_number: str) -> bool:
-    """This function validates a credit card number."""
-    # 1. Change datatype to list[int]
-    card_number = [int(num) for num in card_number]
-
-    # 2. Remove the last digit:
-    checkDigit = card_number.pop(-1)
-
-    # 3. Reverse the remaining digits:
-    card_number.reverse()
-
-    # 4. Double digits at even indices
-    card_number = [num * 2 if idx % 2 == 0
-                   else num for idx, num in enumerate(card_number)]
-
-    # 5. Subtract 9 at even indices if digit is over 9
-    # (or you can add the digits)
-    card_number = [num - 9 if idx % 2 == 0 and num > 9
-                   else num for idx, num in enumerate(card_number)]
-
-    # 6. Add the checkDigit back to the list:
-    card_number.append(checkDigit)
-
-    # 7. Sum all digits:
-    checkSum = sum(card_number)
-
-    # 8. If checkSum is divisible by 10, it is valid.
-    return checkSum % 10 == 0
-
-def validate_user_name(name: str) -> bool:
-    """This function validates a cardholder name."""
-    if "meow" not in name:
-        return False
-    return True
-
-def validate_cvv(cvv: str) -> bool:
-    """This function validated the card cvv"""
-    if len(cvv) == 3 and cvv.isnumeric() and int(cvv) < 100:
-        return True
-    return False
+    # Return template
+    return render_template('./lotw.html', has_llamas=has_llamas, country=country)
---
+++ validation.py
@@ -0,0 +1,41 @@
+class CreditCardValidator:
+    def validate_card_number(card_number: str) -> bool:
+        """This function validates a credit card number."""
+        # 1. Change datatype to list[int]
+        card_number = [int(num) for num in card_number]
+
+        # 2. Remove the last digit:
+        checkDigit = card_number.pop(-1)
+
+        # 3. Reverse the remaining digits:
+        card_number.reverse()
+
+        # 4. Double digits at even indices
+        card_number = [num * 2 if idx % 2 == 0
+                    else num for idx, num in enumerate(card_number)]
+
+        # 5. Subtract 9 at even indices if digit is over 9
+        # (or you can add the digits)
+        card_number = [num - 9 if idx % 2 == 0 and num > 9
+                    else num for idx, num in enumerate(card_number)]
+
+        # 6. Add the checkDigit back to the list:
+        card_number.append(checkDigit)
+
+        # 7. Sum all digits:
+        checkSum = sum(card_number)
+
+        # 8. If checkSum is divisible by 10, it is valid.
+        return checkSum % 10 == 0
+
+    def validate_cardholder_name(name: str) -> bool:
+        """This function validates a cardholder name."""
+        if "meow" not in name:
+            return False
+        return True
+
+    def validate_cvv(cvv: str) -> bool:
+        """This function validated the card cvv"""
+        if len(cvv) == 3 and cvv.isnumeric() and int(cvv) < 100:
+            return True
+        return False
